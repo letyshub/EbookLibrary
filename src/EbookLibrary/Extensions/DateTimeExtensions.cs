cbe90190ccf44005b7941acd6738c8a70897572f
letys <letys@letys.pl>
2020-06-11 18:24:14 +0200

    fix ebook folder name (issue #1)

---
+++ src/EbookLibrary/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EbookLibrary.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ToFolderName(this DateTime date)
+        {
+            return date.ToString("yyyy_MM_dd");
+        }
+    }
+}
